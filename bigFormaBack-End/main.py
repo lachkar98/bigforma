9e21013ff368b0da431ccdd5277c2d9177a82a14
Bilal Boudjema <bilal.boudjema20@gmail.com>
2023-12-09 00:13:27 +0100

    back end

--- bigFormaBack-End/main.py
+++ bigFormaBack-End/main.py
@@ -1,14 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from bd import Db
-app = Flask(__name__)
 
+app = Flask(__name__)
 database = Db()
 
 @app.route('/domain-form', methods=['GET'])
 def get_domain_forms():
-    database.fetch_domain_formats()
-    data = {'response': 'les donnees sont affichee sur votre console python'}
-    return jsonify(data)
+    data = database.fetch_domain_formats()
+    return jsonify({'formations': data})
 
 if __name__ == '__main__':
     app.run(debug=True)
